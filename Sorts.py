62d69bb8a7559e2572ee9c328211dc3ccffea8f1
Luis Pino <lapy0110@gmail.com>
2018-10-10 13:22:07 -0400

    Agregada la implementación de InsertionSort

--- Sorts.py
+++ Sorts.py
@@ -1,3 +1,17 @@
 # Sorts.py
 # Autor: Luis Pino (15-11138) y Félix Arnos (15-10088)
 # Descripcion: Implementacion de los algoritmos InsertionSort y MergeSort
+
+# Ordenamiento por inserción
+def InsertionSort(A:list,P:int,R:int):
+    for j in range (P+1,R):
+        key=A[j]  #Elemento a Insertar
+        #
+        i=j-1
+        while (i>=1 and A[i]>key):
+            A[i+1] = A[i]
+            i = i-1
+        A[i+1] = key
+    return(A)
+
+#Ordenamiento por unión
